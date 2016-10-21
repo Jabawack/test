1b0b84f9951c38f340463aeaa301c328831376ca
TK <tk.hfes@gmail.com>
2016-10-20 23:29:29 -0700

    add WHERE to sql

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var restify = require('restify');
 var pg = require('pg');
 var port = process.env.PORT || 3000;
-
 var server = restify.createServer();
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 server.use(restify.authorizationParser());
 server.use(restify.CORS({
-    origins: ['*'],
-    credentials: true
+  origins: ['*']
+  , credentials: true
 }));
-
-server.get('/db', function(req, res, next) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+server.get('/db', function (req, res, next) {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
     //client.query('SELECT * FROM users WHERE email=$1', [req.username], function(err, result) {
-    client.query('SELECT * FROM users', function(err, result) {
+    client.query('SELECT * FROM users WHERE uid=$1', req.params['uid'], function (err, result) {
+      console.log(req.params['uid']);
+      console.log(result);
       done();
       if (err) {
         return next(new restify.InternalServerError('Error querying your db'));
@@ -24,19 +24,16 @@ server.get('/db', function(req, res, next) {
     });
   });
 });
-
-server.get('/', function(req, res, next) {
-	res.send('hello');
+server.get('/', function (req, res, next) {
+  res.send('hello');
 });
 
 function respond(req, res, next) {
   res.send('hello ' + req.params.name);
   next();
 }
-
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
-
-server.listen(port, function() {
+server.listen(port, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
--- schema.sql
+++ schema.sql
@@ -1,18 +1,19 @@
 CREATE TABLE users (
   id bigserial,
-  userid text primary key,
-  password text NOT null,
+  uid text primary key,
+  pw text NOT null,
   email text NOT null,
   q1 bigint NOT null,
   a1 text NOT null,
   q2 bigint NOT null,
   a2 text NOT null,
   mobile text NOT null,
-  address text NOT null,
-  comments text NOT null,
+  addr text NOT null,
+  interest text NOT null,
   created_at timestamptz,
   updated_at timestamptz,
   deleted_at timestamptz
 );
 
-insert into users (userid, password, email, q1, a1, q2, a2, mobile, address, comments) values ('abc', 'password', 'email@email.com', 1, 'abc', 2, 'abc', '123-456-7890','abc','abc');
+insert into users (uid, pw, email, q1, a1, q2, a2, mobile, addr, interest) values ('abc', 'password', 'email@email.com', 1, 'abc', 2, 'abc', '123-456-7890','abc','abc');
+insert into users (uid, pw, email, q1, a1, q2, a2, mobile, addr, interest) values ('abcd', 'password', 'email@email.com', 1, 'abc', 2, 'abc', '123-456-7890','abc','abc');
